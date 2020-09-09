ab080172025d199eac542ccc5ff51d2b3f35db34
gilang <mgfa9802@gmail.com>
2020-09-09 08:54:28 +0700

    First init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('title','home')
+
+
+@section('body')
+
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="card-title">test</div>
+                    </div>
+
+                    <div class="card-body">
+                        test doang cuyy
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+
+    {{-- Base Meta Tags --}}
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    {{-- Custom Meta Tags --}}
+    @yield('meta_tags')
+
+    {{-- Title --}}
+    <title>
+        @yield('title')
+    </title>
+
+    {{-- Base Stylesheets --}}
+    @if(!config('adminlte.enabled_laravel_mix'))
+        <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('vendor/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
+
+        {{-- Configured Stylesheets --}}
+        @include('adminlte::plugins', ['type' => 'css'])
+
+        <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/adminlte.min.css') }}">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    @else
+        <link rel="stylesheet" href="{{ mix(config('adminlte.laravel_mix_css_path', 'css/app.css')) }}">
+    @endif
+
+    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('vendor/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
+
+    {{-- Configured Scripts --}}
+    @include('adminlte::plugins', ['type' => 'js'])
+
+    <script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
+
+</head>
+
+<body class="@yield('classes_body')" @yield('body_data')>
+
+    {{-- Main Navbar --}}
+    @include('layouts.navbar')
+
+    {{-- Main Sidebar --}}
+    @include('layouts.sidebar')
+
+    {{-- Body Content --}}
+    <div class="content-wrapper my-4">
+        @yield('body')
+    </div>
+
+
+</body>
+
+</html>

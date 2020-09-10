0e163372d2adde8944b3d0b9fd3b380c7181ec23
gilang <mgfa9802@gmail.com>
2020-09-10 13:04:48 +0700

    Change icon in sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -24,50 +24,57 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item has-treeview {{ request()->is('settings*') ? 'menu-open' : '' }}">
-            <a href="#" class="nav-link {{ request()->is('settings*') ? 'active' : '' }}">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Settings
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="{{ route('user.index') }}" class="nav-link {{ request()->is('settings/user*') ? 'active' : '' }}">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>User</p>
+            <li class="nav-item">
+                <a href="{{ route('home') }}" class="nav-link {{ request()->is('home') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    Dashboard
                 </a>
-              </li>
-              <li class="nav-item">
-                <a href="{{ route('role.index') }}" class="nav-link {{ request()->is('settings/role*') ? 'active' : '' }}">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Role</p>
+            </li>
+
+            <li class="nav-item has-treeview {{ request()->is('settings*') ? 'menu-open' : '' }}">
+                <a href="#" class="nav-link {{ request()->is('settings*') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-cog"></i>
+                    <p>
+                        Settings
+                        <i class="right fas fa-angle-left"></i>
+                    </p>
                 </a>
-              </li>
-              <li class="nav-item">
-                <a href="{{ route('permission.index') }}" class="nav-link {{ request()->is('settings/permission*') ? 'active' : '' }}">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Permission</p>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="{{ route('user.index') }}" class="nav-link {{ request()->is('settings/user*') ? 'active' : '' }}">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>User</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('role.index') }}" class="nav-link {{ request()->is('settings/role*') ? 'active' : '' }}">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Role</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('permission.index') }}" class="nav-link {{ request()->is('settings/permission*') ? 'active' : '' }}">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Permission</p>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-list"></i>
+                    <p>
+                        Simple Link
+                        <span class="right badge badge-danger">New</span>
+                    </p>
                 </a>
-              </li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>
-                Simple Link
-                <span class="right badge badge-danger">New</span>
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="dropdown-item" href="{{ route('logout') }}"onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                <i class="fas fa-power-off nav-icon"></i>
-                &nbsp;&nbsp;
-                <i class="icon-switch2"></i> {{ __('Logout') }}
-            </a>
+            </li>
+            <li class="nav-item">
+                <a class="dropdown-item" href="{{ route('logout') }}"onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    <i class="fas fa-power-off nav-icon"></i>
+                    &nbsp;&nbsp;
+                    <i class="icon-switch2"></i> {{ __('Logout') }}
+                </a>
 
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                 @csrf

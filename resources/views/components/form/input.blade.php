c4f04d96d40cbf8ad53fba1aa68954fa0f1ccbf2
gilang <mgfa9802@gmail.com>
2020-09-12 11:16:42 +0700

    Delete div in component

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,9 +1,7 @@
-<div>
-    <div class="form-group {{ $class ?? '' }}">
-        <label>{{ $label }}</label>
-        <input type="{{ $type }}" name="{{ $name }}" value="{{ old($name) }}" class="form-control form-control-sm" placeholder="Isi Input {{ $name }}">
-        @error($name)
-            <span class="text-danger">{{ $name }} Harus diisi</span>
-        @enderror
-    </div>
+<div class="form-group {{ $class ?? '' }}">
+    <label>{{ $label }}</label>
+    <input type="{{ $type }}" name="{{ $name }}" value="{{ old($name) }}" class="form-control form-control-sm" placeholder="Isi Input {{ $name }}">
+    @error($name)
+        <span class="text-danger">{{ $name }} Harus diisi</span>
+    @enderror
 </div>
--- resources/views/components/form/select.blade.php
+++ resources/views/components/form/select.blade.php
@@ -1,14 +1,12 @@
-<div>
-    <div class="form-group {{ $class ?? '' }}">
-        <label>{{ $label }}</label>
-        <select name="{{ $name }}" id="{{ $id ?? '' }}" class="form-control form-control-sm select2">
-            <option value="">...</option>
-            @foreach ($data as $key => $value)
-                <option value="{{ $key }}">{{ $value }}</option>
-            @endforeach
-        </select>
-        @error($name)
-            <span class="text-danger">{{ $name }} Harus Dipilih</span>
-        @enderror
-    </div>
+<div class="form-group {{ $class ?? '' }}">
+    <label>{{ $label }}</label>
+    <select name="{{ $name }}" id="{{ $id ?? '' }}" class="form-control form-control-sm select2">
+        <option value="">...</option>
+        @foreach ($data as $key => $value)
+            <option value="{{ $key }}">{{ $value }}</option>
+        @endforeach
+    </select>
+    @error($name)
+        <span class="text-danger">{{ $name }} Harus Dipilih</span>
+    @enderror
 </div>
